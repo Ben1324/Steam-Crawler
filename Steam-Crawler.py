0fe93cbeef3cce230d6f938e0906bf2ea4cead1f
Ben1324 <112476708+Ben1324@users.noreply.github.com>
2022-08-30 16:47:20 +0100

    Update Steam-Crawler.py

--- Steam-Crawler.py
+++ Steam-Crawler.py
@@ -17,7 +17,7 @@ def find_replace(game_ID, find, replace, number = "0"):
     text.close()
 
 
-def write_on_file(game_ID, type, write = "", number = "0", encoding = "utf-8-sig"):
+def write_on_file(game_ID, type, write = "", number = "0"):
     steam_reviews = open(f"steam_review_page_{game_ID}_{number}.json", mode= type)
     steam_reviews.write(write)
     steam_reviews.close()
@@ -33,25 +33,26 @@ def check_file(game_ID, number):
     find_replace(game_ID, ",]", "]", number)
     find_replace(game_ID, ", ]", "]", number)
     find_replace(game_ID, "],", "]", number)
+    find_replace(game_ID, ",,", ",", number)
+    find_replace(game_ID, "]]", "]", number)
     
 
 def normal_sort(game_ID):
     API_json_text = get_steam_reviews(game_ID)
-    total = get_review_stats(API_json_text)["total_reviews"]
-    current = get_review_stats(API_json_text)["num_reviews"]
-    pages = int(total)//int(current) + 1
     counter = 0
     length = 0
     new_counter = 0
     file_number = 0
+    old_cursor = 1
+    cursor = None
     write_on_file(game_ID, "w", "[", file_number)
-    while counter <= pages and length < total:
-        num_of_reviews = len(API_json_text["reviews"])
+    while True:
         if counter == 0:
             API_json_text = get_steam_reviews(game_ID)
         else:
             cursor = API_json_text["cursor"]
             API_json_text = get_steam_reviews(game_ID, urllib.parse.quote(cursor))
+        num_of_reviews = len(API_json_text["reviews"])
         length += num_of_reviews
         json_file = (json.dumps(API_json_text["reviews"])[1:-1] )
         if new_counter >= 50:
@@ -59,39 +60,60 @@ def normal_sort(game_ID):
             check_file(game_ID, file_number)
             new_counter = 0
             file_number = file_number + 1
-            print(f"file {file_number}: completed")
+            print(f"file {file_number - 1}: completed")
             write_on_file(game_ID, "w", "[", file_number)
         else:
             write_on_file(game_ID, "a", json_file + ",", file_number)
-        print(f"{counter + 1} reviews printed")
+        print(f"{counter + 1} reviews")
+        if cursor == old_cursor:
+            check_file(game_ID, file_number)
+            break
+        else:
+            old_cursor = cursor
         counter = counter + 1
         new_counter = new_counter + 1
-    check_file(game_ID, file_number)
+    path = "."
+    newer_counter = 0
+    absolute_path = os.path.abspath(path)
+    for filename in os.listdir(absolute_path):
+        if os.path.isfile(os.path.join(absolute_path, filename)) and filename.endswith(".json") and filename.startswith('steam'):
+            check_file(game_ID, newer_counter)
+            newer_counter += 1
 
 
 def sort_by_date(recent_unix, distant_unix, game_ID):
     API_json_text = get_steam_reviews(game_ID)
     counter = 0
+    timer = 1
+    old_cursor = 0
+    cursor = None
     num_of_reviews = 0
     write_on_file(game_ID, "w", "[")
-    while True:
+
+    while timer != 0:
+
         if counter != 0:
+            old_cursor = cursor
             cursor = API_json_text["cursor"]
             API_json_text = get_steam_reviews(game_ID, urllib.parse.quote(cursor))
+
         for individual_review in API_json_text["reviews"]:
             time = individual_review["timestamp_created"]
-            if time >= distant_unix and time <= recent_unix:
+
+            if time < distant_unix or cursor == old_cursor:
+                print(num_of_reviews)
+                write_on_file(game_ID, "a", "]")
+                find_replace(game_ID, "][", ", ")
+                find_replace(game_ID, ",]", "]")
+                print("File sorted!")
+                return
+
+            elif time <= recent_unix:
                 print("Printing Steam reviews")
                 json_file = (json.dumps(individual_review) + ",")
                 write_on_file(game_ID, "a", json_file)
                 num_of_reviews = num_of_reviews + 1
-        if time <= distant_unix:
-            print(num_of_reviews)
-            write_on_file(game_ID, "a", "]")
-            find_replace(game_ID, "][", ", ")
-            find_replace(game_ID, ",]", "]")
-            print("File sorted!")
-            return
+
         counter = counter + 1
 
 def start_code(game_ID, recent_date, distant_date, user_input):
@@ -110,15 +132,17 @@ def start_code(game_ID, recent_date, distant_date, user_input):
 
 game_ID = os.getenv("STEAM_ID")
 if game_ID == None:
-    game_ID = 1044720
+    game_ID = 1044720 # marvel spiderman remastered
 
 # year, month, day, hour, minute
-recent_date = datetime.datetime(2022, 8, 15, 0, 0)
-distant_date = datetime.datetime(2022, 7, 29, 0, 0)
+recent_date = datetime.datetime(2022, 8, 30, 0, 0)
+distant_date = datetime.datetime(2022, 8, 15, 0, 0)
 
 # input = 1 is sorting reviews normoly orderd by date
 # input = 2 is getting reviews within a specific time frame
-user_input = 1
+# input 2 doesnt work if timespan specified conflick with game release date.
+user_input = 2
 
 start_code(game_ID, recent_date, distant_date, user_input)
 print("Steam reviews gathered")
+
